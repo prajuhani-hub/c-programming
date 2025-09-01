97554ae14b588976626d8c25ab82fb56ca089bbd
prajuhani-hub <prajuhani@gmail.com>
2025-09-01 12:07:00 +0530

    Add files via upload

---
+++ prajesh.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+  char str[20];
+	printf("hello world");
+	scanf("%s,str");
+	printf("hello world,%s\n",str);
+	return 0;
+}
---
+++ prajesh2.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	char str[20];
+	printf("today is monday");
+	scanf("%s,str");
+	printf("today is monday,%s",str);
+	return 0;
+}
