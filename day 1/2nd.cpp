889066413b6ea5927a02f5aa96399bd0516f45b0
prajuhani-hub <prajuhani@gmail.com>
2025-10-26 20:57:34 +0530

    Add files via upload

---
+++ day 1/2nd.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+	char word[50];
+	scanf("%s",word);
+	printf("%s\n",word);
+	return 0;
+}
---
+++ day 1/3rd.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	char sentence[100];
+	printf("%s",sentence);
+	gets(sentence);
+	printf("%s\n",sentence);
+	return 0;
+}
