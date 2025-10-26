4758f4a25404a853ebc7e0386a39c5f254a35df5
prajuhani-hub <prajuhani@gmail.com>
2025-10-26 22:10:24 +0530

    Add files via upload

---
+++ day 2/5th.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	printf("size of char:%lu\n",sizeof(char));
+	printf("size of int:%lu\n",sizeof(int));
+	printf("size of float:%lu\n",sizeof(float));
+	printf("size of double:%lu\n",sizeof(double));
+	return 0;
+}
---
+++ day 2/6th.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a,b,c,d,e;
+	scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+	printf("mark in subject 1:%d\n",a);
+	printf("mark in subject 2:%d\n",b);
+	printf("mark in subject 3:%d\n",c);
+	printf("mark in subject 4:%d\n",d);
+	printf("mark in subject 5:%d\n",e);
+	return 0;
+}
